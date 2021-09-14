62258b04b0d30178db086f3e641f78149ebd138e
Nicolas <nicksiuxs@gmail.com>
2021-09-14 09:53:49 -0500

    ADD: routes and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "src/pages/Home/Home";
+import Header from "src/components/Header/Header";
+import PokemonDetail from "src/pages/PokemonDetail/PokemonDetail";
+
 import "./App.css";
 
 function App() {
-  return <h1>Hola mundo</h1>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/home/:index">
+          <PokemonDetail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
