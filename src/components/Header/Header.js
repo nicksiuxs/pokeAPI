26fce328ec9a06658bcbbb2bfa2894e6b042c41f
Nicolas <nicksiuxs@gmail.com>
2021-09-14 09:38:44 -0500

    ADD: header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import PokemonLogo from "src/assets/pokemon-logo.png";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Link to="/">
+        <img src={PokemonLogo} className="header-logo" alt="pokemon" />
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
