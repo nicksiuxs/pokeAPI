d7dbd454d8ca3042c4faf0d9ba752eac4441ee61
Nicolas <nicksiuxs@gmail.com>
2021-09-14 10:26:51 -0500

    ADD: checkpoint pokemon detail

--- src/pages/PokemonDetail/PokemonDetail.js
+++ src/pages/PokemonDetail/PokemonDetail.js
@@ -9,35 +9,42 @@ const PokemonDetail = () => {
   let { index } = useParams();
   const pokemon = firstCharge[index];
 
-  const { name, sprites, abilities, moves } = pokemon;
-  const { front_default, back_default } = sprites;
   console.log(pokemon);
+
+  const { name, sprites, abilities, moves } = pokemon;
+  const { front_default, back_default, front_shiny } = sprites;
   return (
     <div className="pokemon-detail">
-      <h2>{name}</h2>
-      <ul>
+      <h2 className="pokemon-detail__title">{name}</h2>
+      <ul className="pokemon-detail__images">
         <li>
           <img src={front_default} alt="front" />
         </li>
         <li>
           <img src={back_default} alt="back" />
         </li>
+        <li>
+          <img src={front_shiny} alt="shinny" />
+        </li>
       </ul>
-      <div>
-        <h3>Abilities</h3>
-        <ul>
-          {abilities.map((ability, key) => {
-            return <li>{ability.ability.name}</li>;
-          })}
-        </ul>
-      </div>
-      <div>
-        <h3>Moves</h3>
-        <ul>
-          {moves.map((move, key) => {
-            return <li>{move.move.name}</li>;
-          })}
-        </ul>
+
+      <div className="pokemon-detail__about">
+        <div className="pokemon-detail__abilities">
+          <h3>Abilities</h3>
+          <ul>
+            {abilities.map((ability, key) => {
+              return <li key={key}>{ability.ability.name}</li>;
+            })}
+          </ul>
+        </div>
+        <div className="pokemon-detail__moves">
+          <h3>Moves</h3>
+          <ul>
+            {moves.map((move, key) => {
+              return <li key={key}>{move.move.name}</li>;
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
