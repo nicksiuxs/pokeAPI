0fd98519fe16e3a38999dce83a4c8f9968a564b6
Nicolas <nicksiuxs@gmail.com>
2021-09-14 15:54:19 -0500

    ADD: move item component

--- src/components/MovesList/MovesList.js
+++ src/components/MovesList/MovesList.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./MovesList.css";
 
-const MovesList = ({ moves, title }) => {
+const MovesList = ({ moves, title, name }) => {
   return (
     <div className="moves-list">
+      <Link to={"/move/" + name}>See more</Link>
       <h3 className="moves-list__title">{title}</h3>
+
       <ul className="moves-list__moves">
         {moves.map((move, key) => {
           return <li key={key}>{move.move.name}</li>;
