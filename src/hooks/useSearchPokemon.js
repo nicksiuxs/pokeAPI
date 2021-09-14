ccbf34d019d59c4c6e10c37dd94bb64341bc1c72
Nicolas <nicksiuxs@gmail.com>
2021-09-14 10:53:08 -0500

    ADD: handling errors

--- src/hooks/useSearchPokemon.js
+++ src/hooks/useSearchPokemon.js
@@ -1,25 +1,29 @@
 import { useState, useEffect } from "react";
 
 const useSearchPokemon = (name) => {
-	const [pokemon, setPokemon] = useState({
-		name: "",
-		images: "",
-		abilities: [],
-		moves: [],
-	});
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    images: "",
+    abilities: [],
+    moves: [],
+  });
 
-	useEffect(() => {
-		if (name !== "") {
-			fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-				.then((res) => res.json())
-				.then((data) => {
-					const { name, sprites, abilities, moves } = data;
-					setPokemon({ name, images: sprites.front_default, abilities, moves });
-				});
-		}
-	}, [name]);
+  useEffect(() => {
+    if (name !== "") {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((res) => {
+          if (!res.ok) throw Error(res.status);
+          return res.json();
+        })
+        .then((data) => {
+          const { name, sprites, abilities, moves } = data;
+          setPokemon({ name, images: sprites.front_default, abilities, moves });
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [name]);
 
-	return pokemon;
+  return pokemon;
 };
 
 export default useSearchPokemon;
