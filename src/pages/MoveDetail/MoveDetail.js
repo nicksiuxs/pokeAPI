d35d0da4f4baf716101b5493a945acd0200c090b
Nicolas <nicksiuxs@gmail.com>
2021-09-14 15:45:25 -0500

    ADD: move detail

--- src/pages/MoveDetail/MoveDetail.js
+++ src/pages/MoveDetail/MoveDetail.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+
+import MoveItem from "src/components/MoveItem/MoveItem";
+
 import useSearchPokemon from "src/hooks/useSearchPokemon";
+
+import "./MoveDetail.css";
 
 const MoveDetail = () => {
   let { name } = useParams();
-  const pokemon = useSearchPokemon(name);
+
+  const { pokemon } = useSearchPokemon(name);
 
   const { moves } = pokemon;
-  console.log(moves);
+
   return (
-    <div>
-      <ul>
+    <div className="move-detail">
+      <h1 className="move-detail__title">{name} Moves</h1>
+      <ul className="move-detail__list">
         {moves?.map((move, key) => {
-          return <li key={key}>{move.move.name}</li>;
+          return (
+            <li key={key}>
+              <MoveItem text={move.move.name} />
+            </li>
+          );
         })}
       </ul>
     </div>
