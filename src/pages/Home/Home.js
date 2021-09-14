ed93af5726a5a42cfafc592c6d5ee9d0e130d3fe
Nicolas <nicksiuxs@gmail.com>
2021-09-14 10:14:12 -0500

    ADD: home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,27 +4,31 @@ import CardPokemon from "src/components/CardPokemon/CardPokemon";
 
 import firstCharge from "src/assets/poke-json.json";
 
+import PokemonLogo from "src/assets/pokemon-logo.png";
+
 import "./Home.css";
 
 const Home = () => {
-	return (
-		<div className="home">
-			<h1 className="home-title">Poke-api</h1>
-			<ul className="home-pokemons">
-				{firstCharge.map((pokemon, index) => {
-					return (
-						<li key={pokemon.id}>
-							<CardPokemon
-								name={pokemon.name}
-								photo={pokemon.sprites.front_default}
-								index={index}
-							/>
-						</li>
-					);
-				})}
-			</ul>
-		</div>
-	);
+  return (
+    <div className="home">
+      <figure className="home__logo-container">
+        <img src={PokemonLogo} className="home__logo" alt="pokemon" />
+      </figure>
+      <ul className="home__pokemons">
+        {firstCharge.map((pokemon, index) => {
+          return (
+            <li key={pokemon.id}>
+              <CardPokemon
+                name={pokemon.name}
+                photo={pokemon.sprites.front_default}
+                index={index}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
